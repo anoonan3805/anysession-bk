4a47e32c5f11a9af8656b96dd8888ff884eec3ba
Etienne de Bruin <et@10story.com>
2016-12-06 07:34:38 +0000

    landing page and office location pulling from back end

--- common/models/office.js
+++ common/models/office.js
@@ -1,5 +1,67 @@
 'use strict';
-
+var async = require("async");
 module.exports = function(Office) {
 
+//   Office.remoteMethod("getSessions", {
+//         http: {
+//             path: "/getSessions",
+//             verb: "GET"
+//         },
+//         description: "Join sessions to a Office",
+//         //accepts  arguments
+//         accepts: [{
+//             arg: 'req',
+//             type: 'object',
+//             'http': {
+//                 source: 'req'
+//             }
+//         }, {
+//             arg: 'res',
+//             type: 'object',
+//             'http': {
+//                 source: 'res'
+//             }
+//         }],
+//         //we will be returning an object
+//         returns: {
+//             type: 'object',
+//             root: true
+//         }
+//     });
+
+//     Office.getSessions = function(req, res, callback) {
+//         var Session = Office.app.models.Session;
+        
+//         Office.find(req.query.filter, function(error, response) {
+//             if (error) {
+//                 return callback(error);
+//             }
+//             async.forEachOf(response, function(index, i, next) {
+//                 Session.findOne({
+//                     where: {
+//                         officeID: index.id
+//                     }
+//                 }, function(officeError, officeResponse) {
+//                     if (officeError) {
+//                         next(officeError);
+//                     }
+//                     else {
+//                         response[i].sessionInfo = officeResponse;   
+//                         next();
+//                     }
+//                 });
+//             }, function(error) {
+//                 if (error) {
+//                     var error = new Error('async.forEach operation');
+//                     error.statusCode = 500;
+//                     callback(error);
+//                 }
+//                 else {
+//                     callback(null, response);
+//                 }
+//             });
+//         });
+//     };
 };
+
+
